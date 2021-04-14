c955b763ffc5a4a4bcaddca4338f4045d1d8e3e5
Vitor Martins Cruz <vitormartinscruz.dev@gmail.com>
2021-04-14 17:36:13 -0300

    Add results

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
-var fetch = require("node-fetch");
-
-const url = "https://api.github.com/users";
-const client_id = "a7ca7ff94cda14a76a3c";
-const client_secret = "6b9f246b5f8bce6f0ea2d70a5622d7db4a0d72ce";
-
-async function getUser(user) {
-    const data = await fetch(`${url}/${user}?client_id=${client_id}&client_secret=${client_secret}`)
-    
-    const profile = data.json()
-
-    return profile;
+const Url = {
+    link: "https://api.github.com/users",
 }
 
-getUser('vitormrts').then(data => console.log(data))
+const API = {
+    client_id: "",
+    client_secret: ""
+}
+
+const fetchUser = async (user) => {
+    const api_call = await fetch(`${Url.link}/${user}?client_id=${API.client_id}&client_secret=${API.client_secret}`)
+    
+    const data = api_call.json()
+
+    return data;
+}
+
+const showUser = () => {
+    const user = Form.validateUser()
+
+    fetchUser(user).then((res) => console.log(res)).catch(e => console.log(e));
+}
+
+const Form = {
+    user: document.querySelector("#user"),
+
+    validateUser() {
+        let user = Form.getUser();
+
+        if (user.trim() === '') {
+            throw new Error("Por favor, insira um usuário.")
+        }
+
+        return user    
+    },
+
+    getUser() {
+        return Form.user.value
+    }
+}
+
+document.querySelector("#search-button").addEventListener("click", (event) => {
+    event.preventDefault()
+    showUser();
+})
